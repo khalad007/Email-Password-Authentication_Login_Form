384730744fec9edae483a3c64db02b33e976c5ad
khalad007 <www.arifhossain49919@gmail.com>
2023-09-29 10:33:42 -0700

    Navbar & Nested Route Added

---
+++ src/Component/Login/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h3 className='text-4xl'>Login</h3>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/Component/Register/Register.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            <h3 className='text-4xl'>Register Here </h3>
+        </div>
+    );
+};
+
+export default Register;
